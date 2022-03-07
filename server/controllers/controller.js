296a77354f3eee130f34ebaba3ddf482b564c074
Kris Sorensen <Krismsorensen@gmail.com>
2022-03-07 17:04:56 -0500

    delete, and render, and index working
    Co-authored-by: jonoh9257 <jonoh9257@users.noreply.github.com>
    Co-authored-by: dgill05 <dgill05@users.noreply.github.com>
    Co-authored-by: DaneCorpion <DaneCorpion@users.noreply.github.com>

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,5 +1,5 @@
-const { Pool } = require('pg');
-const db = require('../model/model')
+const { Pool } = require("pg");
+const db = require("../model/model");
 const controller = {};
 
 controller.getExpense = (req, res, next) => {
@@ -11,14 +11,13 @@ controller.getExpense = (req, res, next) => {
       // console.log(res.locals.expenses);
       return next();
     });
-  }
-  catch {
+  } catch {
     return next({
-      log: 'fatal error retrieving expenses from database inside controller.getExpense',
-      status: 404
+      log: "fatal error retrieving expenses from database inside controller.getExpense",
+      status: 404,
     });
   }
-}
+};
 
 controller.getBalance = (req, res, next) => {
   const text = `SELECT SUM(amount) FROM expense`;
@@ -30,38 +29,36 @@ controller.getBalance = (req, res, next) => {
       // console.log(res.locals.expenses);
       return next();
     });
-  }
-  catch {
+  } catch {
     return next({
-      log: 'fatal error retrieving balance from database',
-      status: 404
+      log: "fatal error retrieving balance from database",
+      status: 404,
     });
   }
-}
+};
 
 controller.postExpense = (req, res, next) => {
-  const { vendor, amount, category, id} = req.body
+  const { vendor, amount, category, id } = req.body;
 
   // console.log(req.body);
   try {
-    const text = `INSERT INTO expense VALUES( '${vendor}', ${amount}, '2017-12-20','${category}', '${id}');`
+    const text = `INSERT INTO expense VALUES( '${vendor}', ${amount}, '2017-12-20','${category}', '${id}');`;
     res.locals.newExpense = req.body;
     db.query(text, (err, result) => {
       // console.log(result);
       return next();
     });
-  }
-  catch {
+  } catch {
     return next({
-      log: 'fatal error creating new expense in database inside controller.postExpense',
-      status: 404
+      log: "fatal error creating new expense in database inside controller.postExpense",
+      status: 404,
     });
   }
-}
+};
 controller.deleteExpense = (req, res, next) => {
   // const { vendor, amount, category, date} = req.params;
   const { id } = req.params;
-  console.log('req.params for delete', id);
+  console.log("req.params for delete", id);
   try {
     const text = `DELETE FROM expense WHERE id = ${id}`;
     res.locals.oldExpense = req.params;
@@ -69,39 +66,34 @@ controller.deleteExpense = (req, res, next) => {
       // console.log(result);
       return next();
     });
-  }
-  catch {
+  } catch {
     return next({
-      log: 'fatal error deleteing  expense in database inside controller.deleteExpense',
-      status: 404
+      log: "fatal error deleteing  expense in database inside controller.deleteExpense",
+      status: 404,
     });
   }
-}
+};
 
 controller.retrieveLastId = (req, res, next) => {
-  const { vendor, amount, category } = req.body
+  const { vendor, amount, category } = req.body;
   console.log(req.body);
   try {
-    const text = `SELECT id FROM expense ORDER BY id DESC LIMIT 1`
+    const text = `SELECT id FROM expense ORDER BY id DESC LIMIT 1`;
     // console.log('res.locals.index', res.locals.index)
     db.query(text, (err, result) => {
       res.locals.index = result.rows[0].id;
       // console.log(result);
       return next();
     });
-  }
-  catch {
+  } catch {
     return next({
-      log: 'fatal error retriving last index',
-      status: 404
+      log: "fatal error retriving last index",
+      status: 404,
     });
   }
-}
-
+};
 
 module.exports = controller;
-
-
 
 // INSERT INTO table_name(column1, column2, …)
 // VALUES (value1, value2, …);
@@ -111,6 +103,5 @@ module.exports = controller;
 //     Amount int,
 //     Date date,
 //     Category varchar(255),
-//     id int  
+//     id int
 // );
-
--- src/component/Expenses.jsx
+++ src/component/Expenses.jsx
@@ -2,127 +2,142 @@ import React, { useState, useEffect } from "react";
 
 //Expenses Component
 const Expenses = () => {
-    //created state to hold database
-    const [state, setState] = useState([]);
-    //created state to hold net price of expense extries
-    const [currentBalance, setBalance] = useState(0);
-    // created state to hold index of database extries
-    const [currentIndex, setIndex] = useState(0)
+  //created state to hold database
+  const [state, setState] = useState([]);
+  //created state to hold net price of expense extries
+  const [currentBalance, setBalance] = useState(0);
+  // created state to hold index of database extries
+  const [currentIndex, setIndex] = useState(0);
 
-    // upon rendering, sets state to current database 
-    useEffect(() => {
-        fetch('/api/get')
-            .then(response => response.json())
-            .then(data => setState(data))
-    }, [])
+  // upon rendering, sets state to current database
+  useEffect(() => {
+    fetch("/api/get")
+      .then((response) => response.json())
+      .then((data) => setState(data));
+  }, []);
 
-    // //upon rending, sets currentBalance state to current database net price
-    useEffect(() => {
-        fetch('/api/getBalance')
-            .then(response => response.json())
-            .then(data => setBalance(data))
-    }, [])
+  // //upon rending, sets currentBalance state to current database net price
+  useEffect(() => {
+    fetch("/api/getBalance")
+      .then((response) => response.json())
+      .then((data) => setBalance(data));
+  }, []);
 
-    // //upon rending, sets currentIndex to index of last entry of database
-    useEffect(() => {
-        fetch('/api/index')
-            .then(response => response.json())
-            .then(data => setIndex(data))
-    }, [])
+  // //upon rending, sets currentIndex to index of last entry of database
+  useEffect(() => {
+    fetch("/api/index")
+      .then((response) => response.json())
+      .then((data) => setIndex(data));
+  }, []);
 
+  //upon click, submits post requested, updated database with current extry.  Also updates current states with new information
+  function submitClick() {
+    console.log(state);
+    const index = currentIndex;
+    incrementIndex();
+    const vendor = document.getElementById("Vendor").value;
+    const amount = document.getElementById("Amount").value;
+    const category = document.getElementById("Category").value;
+    const date = document.getElementById("Date").value;
 
-    //upon click, submits post requested, updated database with current extry.  Also updates current states with new information
-    function submitClick() {
-        console.log(state)
-        const index = currentIndex
-        incrementIndex()
-        const vendor = document.getElementById('Vendor').value
-        const amount = document.getElementById('Amount').value
-        const category = document.getElementById('Category').value
-        const date = document.getElementById('Date').value
+    const postOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: index,
+        amount: amount,
+        vendor: vendor,
+        category: category,
+        date: date,
+      }),
+    };
+    fetch("/api/expenses", postOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        addBalance(data.amount);
+        return addItem(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-        const postOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id: index, amount: amount, vendor: vendor, category: category, date: date })
-        }
-        fetch('/api/expenses', postOptions)
-            .then(response => response.json())
-            .then(data => {
+  function deleteClick(buttonIndex) {
+    const id = buttonIndex;
+    const deleteOptions = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch(`/api/${id}`, deleteOptions).then((response) => response.json());
+    fetch("/api/get")
+      .then((response) => response.json())
+      .then((data) => setState(data));
+    fetch("/api/getBalance")
+      .then((response) => response.json())
+      .then((data) => setBalance(data));
+    // fetch("/api/index")
+    //   .then((response) => response.json())
+    //   .then((data) => setIndex(data));
+  }
+  // adds new extry to current state
+  function addItem(data) {
+    setState(state.concat(data));
+  }
 
-                addBalance(data.amount)
-                return addItem(data)
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }
+  // adds new expenses price to currentBalance state
+  function addBalance(data) {
+    setBalance(Number(currentBalance) + Number(data));
+  }
+  // increments index state by 1
+  function incrementIndex() {
+    setIndex(currentIndex + 1);
+  }
 
+  return (
+    <>
+      <div>
+        <h1>Hello Expenses Component</h1>
+        <input
+          type="amount"
+          name="Amount"
+          id="Amount"
+          placeholder="Enter Cost"
+        ></input>
+        <input type="vendor" id="Vendor" placeholder="Vendor"></input>
+        <input type="category" id="Category" placeholder="Category"></input>
+        <input type="date" id="Date"></input>
+        <button onClick={submitClick}> Submit</button>
+      </div>
+      <div>
+        <h1>Balance</h1>
+        <h2>{currentIndex}</h2>
+        <li>
+          Balance:
+          {currentBalance}
+        </li>
 
-    function deleteClick(buttonIndex) {
-        const id = buttonIndex
-        const deleteOptions = {
-            method: 'DELETE',
-            headers: { 'Content-Type': 'application/json' },
-        }
-        fetch(`/api/${id}`, deleteOptions)
-            .then(response => response.json())
-        fetch('/api/get')
-            .then(response => response.json())
-            .then(data => setState(data))
-        fetch('/api/getBalance')
-            .then(response => response.json())
-            .then(data => setBalance(data))
-        fetch('/api/index')
-            .then(response => response.json())
-            .then(data => setIndex(data))
-
-    }
-    // adds new extry to current state
-    function addItem(data) {
-        setState(state.concat(data))
-    }
-
-    // adds new expenses price to currentBalance state
-    function addBalance(data) {
-        setBalance(Number(currentBalance) + Number(data))
-    }
-    // increments index state by 1
-    function incrementIndex() {
-        setIndex(currentIndex + 1)
-    }
-
-    return (
-        <>
-            <div>
-                <h1>Hello Expenses Component</h1>
-                <input type='amount' name="Amount" id='Amount' placeholder="Enter Cost"></input>
-                <input type='vendor' id='Vendor' placeholder="Vendor"></input>
-                <input type='category' id='Category' placeholder="Category"></input>
-                <input type='date' id='Date'></input>
-                <button onClick={submitClick}> Submit</button>
-            </div>
-            <div>
-                <h1>Balance</h1>
-                <h2>{currentIndex}</h2>
-                <li>Balance:
-                    {currentBalance}
-                </li>
-
-                {state.map((item, i) => (
-                    <>
-                        <ul>
-                            <li>Price: {item.amount}</li>
-                            <li>Vendor: {item.vendor} </li>
-                            <li>Category: {item.category} </li>
-                            <li>Date:{item.date} </li>
-                            <button id={item.id} onClick={() => { deleteClick(item.id) }}> Remove Expense</button>
-                        </ul>
-                    </>
-                ))}
-            </div>
-        </>
-    );
+        {state.map((item, i) => (
+          <>
+            <ul>
+              <li>Price: {item.amount}</li>
+              <li>Vendor: {item.vendor} </li>
+              <li>Category: {item.category} </li>
+              <li>Date:{item.date} </li>
+              <button
+                id={item.id}
+                onClick={() => {
+                  deleteClick(item.id);
+                }}
+              >
+                {" "}
+                Remove Expense
+              </button>
+            </ul>
+          </>
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default Expenses
+export default Expenses;
