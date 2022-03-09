fdea3ece2eada67f4fe28a71a64309e6716053b7
Mathias Perfumo <mathiasperfumo@Mathiass-MacBook-Pro-2.local>
2022-03-09 16:33:11 -0500

    sidebar in progress

---
+++ src/component/MonthlyDash.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+
+
+const MonthlyDash = () => {
+return (
+    <div>hello
+    {console.log('monthlydash')}
+    </div>
+
+
+)
+
+}
+
+export default MonthlyDash;
